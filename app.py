2689dcba821faf62ce8001d8224984bcb87d85c6
rushil <85025732+rushilhacker1@users.noreply.github.com>
2023-06-18 21:05:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,34 +13,40 @@ app.config['JWT_SECRET_KEY'] = 'your-secret-key'  # Replace with your own secret
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
 
 # Customer Model
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(1000))
     name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(1100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     balance = db.Column(db.Integer, nullable=False)
     location = db.Column(db.String(100), nullable=False)
     destination = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f"Customer(id={self.id}, name='{self.name}', rating={self.rating}, balance={self.balance}, location='{self.location}', destination='{self.destination}')"
+        return f"Customer(id={self.id}, image_url={self.image_url}, name='{self.name}', email={self.email}, password={self.password}, rating={self.rating}, balance={self.balance}, location='{self.location}', destination='{self.destination}')"
 
 
 # Driver Model
 class Driver(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(1000))
     name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(1100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     status = db.Column(db.String(100), nullable=False)
     location = db.Column(db.String(100), nullable=False)
     destination = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f"Driver(id={self.id}, name='{self.name}', rating={self.rating}, status='{self.status}', location='{self.location}', destination='{self.destination}')"
-
+        return f"Driver(id={self.id}, image_url={self.image_url}, name='{self.name}', email={self.email}, password={self.password}, rating={self.rating}, status='{self.status}', location='{self.location}', destination='{self.destination}')"
 
 # Customer Schema
 class CustomerSchema(ma.SQLAlchemyAutoSchema):
@@ -68,12 +75,23 @@ class AuthResource(Resource):
         password = request.json.get('password')
 
         # Authenticate the user (you can use your own authentication logic here)
-        if username != 'admin' or password != 'admin':
+        customer = Customer.query.filter_by(email=username).first()
+        driver = Driver.query.filter_by(email=username).first()
+
+        if not customer and not driver:
             abort(401, message="Invalid credentials")
 
-        # Generate access token
-        access_token = create_access_token(identity=username)
-        return jsonify(access_token=access_token)
+        if customer and bcrypt.check_password_hash(customer.password, password):
+            # Generate access token
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token)
+
+        if driver and bcrypt.check_password_hash(driver.password, password):
+            # Generate access token
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token)
+
+        abort(401, message="Invalid credentials")
 
 
 # Customer Resource
@@ -85,7 +103,6 @@ class CustomerResource(Resource):
             abort(404, message="Customer not found")
         return customer_schema.dump(customer)
 
-    @jwt_required()
     def put(self, customer_id):
         parser = reqparse.RequestParser()
         parser.add_argument("name", type=str, help="Name of the customer is required", required=True)
@@ -93,15 +110,21 @@ class CustomerResource(Resource):
         parser.add_argument("balance", type=int, help="Balance of the customer is required", required=True)
         parser.add_argument("location", type=str, help="Location of the customer is required", required=True)
         parser.add_argument("destination", type=str, help="Destination of the customer is required", required=True)
+        parser.add_argument("email", type=str, help="Email of the customer is required", required=True)
+        parser.add_argument("password", type=str, help="Password of the customer is required", required=True)
         args = parser.parse_args()
 
         customer = Customer.query.get(customer_id)
         if customer:
             abort(409, message="Customer ID already exists")
 
+        hashed_password = bcrypt.generate_password_hash(args['password']).decode('utf-8')
+
         new_customer = Customer(
             id=customer_id,
             name=args['name'],
+            email=args['email'],
+            password=hashed_password,
             rating=args['rating'],
             balance=args['balance'],
             location=args['location'],
@@ -120,6 +143,8 @@ class CustomerResource(Resource):
         parser.add_argument("balance", type=int, help="Balance of the customer")
         parser.add_argument("location", type=str, help="Location of the customer")
         parser.add_argument("destination", type=str, help="Destination of the customer")
+        parser.add_argument("email", type=str, help="Email of the customer")
+        parser.add_argument("password", type=str, help="Password of the customer")
         args = parser.parse_args()
 
         customer = Customer.query.get(customer_id)
@@ -136,6 +161,11 @@ class CustomerResource(Resource):
             customer.location = args['location']
         if args['destination']:
             customer.destination = args['destination']
+        if args['email']:
+            customer.email = args['email']
+        if args['password']:
+            hashed_password = bcrypt.generate_password_hash(args['password']).decode('utf-8')
+            customer.password = hashed_password
 
         db.session.commit()
         return customer_schema.dump(customer)
@@ -163,20 +193,28 @@ class DriverResource(Resource):
     @jwt_required()
     def put(self, driver_id):
         parser = reqparse.RequestParser()
-        parser.add_argument("name", type=str, help="Name of the driver is required", required=True)
-        parser.add_argument("rating", type=int, help="Rating of the driver is required", required=True)
-        parser.add_argument("status", type=str, help="Status of the driver is required", required=True)
-        parser.add_argument("location", type=str, help="Location of the driver is required", required=True)
-        parser.add_argument("destination", type=str, help="Destination of the driver is required", required=True)
+        parser.add_argument("name", type=str, help="Name of the driver is required")
+        parser.add_argument("image_url", type=str, help="Image URL of the driver")
+        parser.add_argument("email", type=str, help="Email of the driver")
+        parser.add_argument("password", type=str, help="Password of the driver")
+        parser.add_argument("rating", type=int, help="Rating of the driver")
+        parser.add_argument("status", type=str, help="Status of the driver")
+        parser.add_argument("location", type=str, help="Location of the driver")
+        parser.add_argument("destination", type=str, help="Destination of the driver")
+
         args = parser.parse_args()
 
         driver = Driver.query.get(driver_id)
         if driver:
             abort(409, message="Driver ID already exists")
 
+        hashed_password = bcrypt.generate_password_hash(args['password']).decode('utf-8')
+
         new_driver = Driver(
             id=driver_id,
             name=args['name'],
+            email=args['email'],
+            password=hashed_password,
             rating=args['rating'],
             status=args['status'],
             location=args['location'],
@@ -190,11 +228,15 @@ class DriverResource(Resource):
     @jwt_required()
     def patch(self, driver_id):
         parser = reqparse.RequestParser()
-        parser.add_argument("name", type=str, help="Name of the driver")
+        parser.add_argument("name", type=str, help="Name of the driver is required")
+        parser.add_argument("image_url", type=str, help="Image URL of the driver")
+        parser.add_argument("email", type=str, help="Email of the driver")
+        parser.add_argument("password", type=str, help="Password of the driver")
         parser.add_argument("rating", type=int, help="Rating of the driver")
         parser.add_argument("status", type=str, help="Status of the driver")
         parser.add_argument("location", type=str, help="Location of the driver")
         parser.add_argument("destination", type=str, help="Destination of the driver")
+
         args = parser.parse_args()
 
         driver = Driver.query.get(driver_id)
@@ -203,6 +245,13 @@ class DriverResource(Resource):
 
         if args['name']:
             driver.name = args['name']
+        if args['image_url']:
+            driver.image_url = args['image_url']
+        if args['email']:
+            driver.email = args['email']
+        if args['password']:
+            hashed_password = bcrypt.generate_password_hash(args['password']).decode('utf-8')
+            driver.password = hashed_password
         if args['rating']:
             driver.rating = args['rating']
         if args['status']:
@@ -254,12 +303,12 @@ class DriverListResource(Resource):
         return '', 204
 
 
-# Add resources to API
 api.add_resource(AuthResource, '/auth')
 api.add_resource(CustomerResource, '/customers/<int:customer_id>')
 api.add_resource(DriverResource, '/drivers/<int:driver_id>')
 api.add_resource(CustomerListResource, '/customers')
 api.add_resource(DriverListResource, '/drivers')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
